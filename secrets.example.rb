f2578f86ab0888b7c7dad523e13138145e27c39a
Daniel Marino <iamdanielmarino@gmail.com>
2024-09-05 15:48:16 -0400

    Add secrets example

---
+++ secrets.example.rb
@@ -0,0 +1,6 @@
+ENV['userid'] = 'Your Services id'
+ENV['token'] = 'Your Services application ID'
+ENV['secret'] = 'Your Services secret'
+
+ENV['username'] = 'Your Gmail email'
+ENV['password'] = 'Your Gmail password'
